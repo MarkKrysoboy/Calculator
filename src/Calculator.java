c4cc600854eb57ddaf277a6ca125174c2d90eb95
MarkKrysoboy <heter1209@gmail.com>
2021-12-17 14:31:14 +0300

    Fixed ArithmeticException

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,12 +8,21 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    // BinaryOperator<Integer> devide = (x, y) -> x / y;
+    // Ошибка из-за деления на ноль.
+    BinaryOperator<Integer> devide = (x, y) -> {
+        if (y != 0) {
+            return x / y;
+        } else {
+            System.out.print ("На ноль делить нельзя ");
+            return 0;
+        }
+    };
+    // BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : 0);
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
-    /* К сожалению не понял последние указания из задачи. У меня приведенный выше код исправно работает и
-    ошибок не возникает ни с тернарным оператором, ни с переменной abc.*/
+
 
     Predicate<Integer> isPositive = x -> x > 0;
 
